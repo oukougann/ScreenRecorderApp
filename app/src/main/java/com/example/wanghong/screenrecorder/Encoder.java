93267d693f1d364fc02b2167fc52f5fe9dfe4c91
oukougann <oukougann@163.com>
2019-05-10 16:02:46 +0900

    first commit

---
+++ app/src/main/java/com/example/wanghong/screenrecorder/Encoder.java
@@ -0,0 +1,17 @@
+package com.example.wanghong.screenrecorder;
+
+import java.io.IOException;
+
+interface Encoder {
+    void prepare() throws IOException;
+
+    void stop();
+
+    void release();
+
+    void setCallback(Callback callback);
+
+    interface Callback {
+        void onError(Encoder encoder, Exception exception);
+    }
+}
